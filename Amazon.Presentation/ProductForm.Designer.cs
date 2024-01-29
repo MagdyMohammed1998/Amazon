a5f09bc5a00ddbe2925d2c323f53de77671cf255
dell 5580 <dell 5580@DESKTOP-NP1E6RM>
2024-01-29 13:54:16 +0200

    #update

--- Amazon.Presentation/ProductForm.Designer.cs
+++ Amazon.Presentation/ProductForm.Designer.cs
@@ -29,6 +29,7 @@ namespace Amazon.Presentation
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProductForm));
             button1 = new Button();
             button2 = new Button();
             button3 = new Button();
@@ -55,35 +56,38 @@ namespace Amazon.Presentation
             // 
             // button1
             // 
+            button1.BackColor = Color.FromArgb(0, 192, 0);
             button1.Font = new Font("Times New Roman", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             button1.Location = new Point(39, 598);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 4;
             button1.Text = "Add";
-            button1.UseVisualStyleBackColor = true;
+            button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
             // 
             // button2
             // 
+            button2.BackColor = Color.Red;
             button2.Font = new Font("Times New Roman", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             button2.Location = new Point(158, 598);
             button2.Name = "button2";
             button2.Size = new Size(75, 23);
             button2.TabIndex = 6;
             button2.Text = "Delete";
-            button2.UseVisualStyleBackColor = true;
+            button2.UseVisualStyleBackColor = false;
             button2.Click += button2_Click;
             // 
             // button3
             // 
+            button3.BackColor = Color.Yellow;
             button3.Font = new Font("Times New Roman", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             button3.Location = new Point(281, 598);
             button3.Name = "button3";
             button3.Size = new Size(75, 23);
             button3.TabIndex = 7;
             button3.Text = "Update";
-            button3.UseVisualStyleBackColor = true;
+            button3.UseVisualStyleBackColor = false;
             button3.Click += button3_Click;
             // 
             // label3
@@ -98,13 +102,14 @@ namespace Amazon.Presentation
             // 
             // button4
             // 
+            button4.BackColor = Color.Blue;
             button4.Font = new Font("Times New Roman", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             button4.Location = new Point(415, 598);
             button4.Name = "button4";
             button4.Size = new Size(75, 23);
             button4.TabIndex = 11;
             button4.Text = "GetAll";
-            button4.UseVisualStyleBackColor = true;
+            button4.UseVisualStyleBackColor = false;
             button4.Click += button4_Click;
             // 
             // button7
@@ -208,13 +213,14 @@ namespace Amazon.Presentation
             // 
             // Search
             // 
+            Search.BackColor = Color.White;
             Search.Font = new Font("Times New Roman", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             Search.Location = new Point(773, 598);
             Search.Name = "Search";
             Search.Size = new Size(70, 23);
             Search.TabIndex = 27;
             Search.Text = "Search";
-            Search.UseVisualStyleBackColor = true;
+            Search.UseVisualStyleBackColor = false;
             Search.Click += Search_Click;
             // 
             // comboBox1
@@ -228,7 +234,8 @@ namespace Amazon.Presentation
             // pictureBox1
             // 
             pictureBox1.BackColor = Color.White;
-            pictureBox1.BackgroundImageLayout = ImageLayout.None;
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
             pictureBox1.Location = new Point(26, 344);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(252, 161);
@@ -251,7 +258,7 @@ namespace Amazon.Presentation
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.Lavender;
+            BackColor = Color.Teal;
             ClientSize = new Size(1033, 712);
             Controls.Add(label2);
             Controls.Add(pictureBox1);
--- Amazon.Presentation/adminPanle.Designer.cs
+++ Amazon.Presentation/adminPanle.Designer.cs
@@ -33,6 +33,8 @@
             label2 = new Label();
             label3 = new Label();
             label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
             SuspendLayout();
             // 
             // label1
@@ -50,7 +52,7 @@
             // 
             label2.AutoSize = true;
             label2.ForeColor = Color.Red;
-            label2.Location = new Point(13, 134);
+            label2.Location = new Point(13, 167);
             label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
             label2.Size = new Size(91, 19);
@@ -61,7 +63,7 @@
             // 
             label3.AutoSize = true;
             label3.ForeColor = Color.Red;
-            label3.Location = new Point(2, 186);
+            label3.Location = new Point(2, 128);
             label3.Margin = new Padding(4, 0, 4, 0);
             label3.Name = "label3";
             label3.Size = new Size(102, 19);
@@ -72,12 +74,36 @@
             // 
             label4.AutoSize = true;
             label4.ForeColor = Color.Red;
-            label4.Location = new Point(25, 242);
+            label4.Location = new Point(13, 216);
             label4.Margin = new Padding(4, 0, 4, 0);
             label4.Name = "label4";
             label4.Size = new Size(82, 19);
             label4.TabIndex = 3;
             label4.Text = "Add Admin";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.ForeColor = Color.Red;
+            label5.Location = new Point(13, 267);
+            label5.Margin = new Padding(4, 0, 4, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(63, 19);
+            label5.TabIndex = 4;
+            label5.Text = "Reports";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = SystemColors.Highlight;
+            label6.Font = new Font("Times New Roman", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(355, 9);
+            label6.Margin = new Padding(4, 0, 4, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(155, 24);
+            label6.TabIndex = 5;
+            label6.Text = "Welcome Admin";
             // 
             // adminPanle
             // 
@@ -86,12 +112,14 @@
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Stretch;
             ClientSize = new Size(1029, 570);
+            Controls.Add(label6);
+            Controls.Add(label5);
             Controls.Add(label4);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
             Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            Margin = new Padding(4, 4, 4, 4);
+            Margin = new Padding(4);
             Name = "adminPanle";
             Text = "adminPanle";
             ResumeLayout(false);
@@ -104,5 +132,7 @@
         private Label label2;
         private Label label3;
         private Label label4;
+        private Label label5;
+        private Label label6;
     }
 }
